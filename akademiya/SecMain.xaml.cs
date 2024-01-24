d9f0fea950c1ea057700ae15b65808ab788933b0
danya <danya@3205EC09>
2024-01-24 12:09:15 +0300

    Добавьте файлы проекта.

---
+++ akademiya/SecMain.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace akademiya
+{
+    /// <summary>
+    /// Логика взаимодействия для SecMain.xaml
+    /// </summary>
+    public partial class SecMain : Window
+    {
+        public SecMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
